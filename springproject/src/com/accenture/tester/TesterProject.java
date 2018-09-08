58cf6d69a98f81caa1e0d180cb353a1a4176776d
Vivek Bhatia <vivek@gmail.com>
2018-09-08 17:50:32 +0530

    SpringCore

---
+++ springproject/src/com/accenture/tester/TesterProject.java
@@ -0,0 +1,19 @@
+package com.accenture.tester;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.accenture.entity.Employee;
+
+public class TesterProject {
+
+	public static void main(String[] args) {
+
+    ApplicationContext context=new ClassPathXmlApplicationContext("applicationcontext.xml");
+    Employee employee=(Employee) context.getBean("employee");
+    System.out.println("Employee id "+employee.getId());
+    System.out.println("Employee name "+employee.getName());
+    System.out.println("Employee email "+employee.getEmail());
+	}
+
+}
